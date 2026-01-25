d0a5b729f7cfcc34e3803333ea00a726bf1bb4d7
Dilsanka Sajith <Dilsanka39@gmail.com>
2026-01-25 11:50:02 +0530

    Before connecting with the database

---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,79 @@
+import Link from 'next/link';
+import { MaxWidthWrapper } from './max-width-wrapper';
+import { Button, buttonVariants } from '@/components/ui/button';
+import { ArrowRight } from 'lucide-react';
+import { SignOutButton } from '@clerk/nextjs';
+import { currentUser } from '@clerk/nextjs/server';
+
+const Navbar = async () => {
+  const user = await currentUser();
+
+  return (
+    <nav className="sticky z-100 h-16 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/80 backdrop-blur-lg transition-all">
+      <MaxWidthWrapper>
+        <div className="flex h-16 items-center justify-between">
+          <Link href="/" className="flex z-40 font-semibold">
+            Paw<span className="text-brand-700">Match</span>
+          </Link>
+
+          <div className="h-full flex items-center space-x-4">
+            <Link
+              href="/dashboard"
+              className={buttonVariants({
+                size: 'sm',
+                variant: 'ghost',
+              })}
+            >
+              Report Stray ❤️
+            </Link>
+            {user ? (
+              <>
+                <SignOutButton>
+                  <Button size="sm" variant="ghost">
+                    Sign out
+                  </Button>
+                </SignOutButton>
+
+                <Link
+                  href="/dashboard"
+                  className={buttonVariants({
+                    size: 'sm',
+                    className: 'flex items-center gap-1',
+                  })}
+                >
+                  Dashboad <ArrowRight className="ml-1.5 size-4" />
+                </Link>
+              </>
+            ) : (
+              <>
+                <Link
+                  href="/sign-in"
+                  className={buttonVariants({
+                    size: 'sm',
+                    variant: 'ghost',
+                  })}
+                >
+                  Sign in
+                </Link>
+
+                <div className="h-8 w-px bg-gray-200" />
+
+                <Link
+                  href="/sign-up"
+                  className={buttonVariants({
+                    size: 'sm',
+                    className: 'flex items-center gap-1.5',
+                  })}
+                >
+                  Sign up <ArrowRight className="size-4" />
+                </Link>
+              </>
+            )}
+          </div>
+        </div>
+      </MaxWidthWrapper>
+    </nav>
+  );
+};
+
+export default Navbar;
